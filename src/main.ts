ac255f5235d52a2c6a72fb68716fb5218136abc8
vthanhdong05 <v.thanhdong05@gmail.com>
2026-05-27 05:51:18 +0700

    chore(app): setup main bootstrap, swagger

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import { initApp } from './init';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   initApp(app);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
