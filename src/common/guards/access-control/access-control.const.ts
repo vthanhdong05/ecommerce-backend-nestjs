000ed851affe2e82560b801f25169339f3fc6307
vthanhdong05 <v.thanhdong05@gmail.com>
2026-06-03 06:27:27 +0700

    feat(user): add isSuperAdmin and isExistPermissionKey methods

---
+++ src/common/guards/access-control/access-control.const.ts
@@ -0,0 +1,8 @@
+// chuẩn hóa các hành động CRUD (Create, Read, Update, Delete) và quản lý
+export enum Actions {
+  MANAGE = 'manage',
+  CREATE = 'create',
+  READ = 'read',
+  UPDATE = 'update',
+  DELETE = 'delete',
+}
