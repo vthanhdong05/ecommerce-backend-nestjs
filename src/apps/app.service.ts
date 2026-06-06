06e5e8a818d7840fc9166fcd4e00d1ecaa88984c
vthanhdong05 <v.thanhdong05@gmail.com>
2026-06-06 22:17:39 +0700

    chore(app): add lifecycle hooks to app service

--- src/apps/app.service.ts
+++ src/apps/app.service.ts
@@ -1,7 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BeforeApplicationShutdown,
+  Injectable,
+  OnApplicationBootstrap,
+  OnApplicationShutdown,
+  OnModuleDestroy,
+  OnModuleInit,
+} from '@nestjs/common';
 
 @Injectable()
-export class AppService {
+export class AppService
+  implements
+    OnModuleInit,
+    OnApplicationBootstrap,
+    OnApplicationShutdown,
+    BeforeApplicationShutdown,
+    OnModuleDestroy
+{
+  onModuleInit() {}
+  onApplicationBootstrap() {
+    // console.log('>>> onApplicationBootstrap');
+  }
+  onModuleDestroy() {}
+
+  // only work if app.enableShutdownHooks();
+  beforeApplicationShutdown(_signal?: string) {
+    // console.log('>>> beforeApplicationShutdown');
+  }
+  onApplicationShutdown(_signal?: string) {
+    // console.log('>>> onApplicationShutdown');
+  }
+  health() {
+    return 'OK';
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
